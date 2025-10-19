59230188c6c9905f851c103cb72058b44c09a757
Cyrus NOUKPOZOUNKOU <cyrus.noukpozounkou@ens.uvsq.fr>
2025-10-19 01:09:33 +0100

    Constructeur de la classe NomMachine

--- src/main/java/fr/uvsq/cprog/collex/NomMachine.java
+++ src/main/java/fr/uvsq/cprog/collex/NomMachine.java
@@ -1,5 +1,18 @@
 package fr.uvsq.cprog.collex;
 
 public class NomMachine {
+    private String nomComplet;
+    private String nom;
+    private String domaine;
+
+    public NomMachine(String nomComplet) {
+        if (!nomComplet.contains(".")) {
+            throw new IllegalArgumentException("Nom de machine invalide : " + nomComplet);
+        }
+        this.nomComplet = nomComplet;
+        String[] parts = nomComplet.split("\\.", 2);
+        this.nom = parts[0];
+        this.domaine = parts[1];
+    }
     
 }
