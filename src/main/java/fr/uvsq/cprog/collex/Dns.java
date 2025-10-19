9b18b6dcaa131726ea68444a4b25d7f82863dc8e
Cyrus NOUKPOZOUNKOU <cyrus.noukpozounkou@ens.uvsq.fr>
2025-10-19 02:30:50 +0100

    Ajout du constructeur de Dns

--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -1,5 +1,49 @@
 package fr.uvsq.cprog.collex;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 public class Dns {
+    private List<DnsItem> items = new ArrayList<>();
+    private Path filePath;
+
+    public Dns() throws IOException {
+        
+        Properties props = new Properties();
+        try (var fis = Files.newInputStream(Path.of("config.properties"))) {
+            props.load(fis);
+        }
+
+        String dnsFile = props.getProperty("dns.file");
+        if (dnsFile == null) {
+            throw new RuntimeException("Propriété 'dns.file' manquante dans config.properties");
+        }
+        this.filePath = Path.of(dnsFile);
+
+        
+        List<String> lignes = Files.readAllLines(filePath);
+
+        
+        for (String ligne : lignes) {
+            String[] parts = ligne.trim().split("\\s+");
+            if (parts.length == 2) {
+                String nom = parts[0];
+                String ip = parts[1];
+                DnsItem item = new DnsItem(new NomMachine(nom), new AdresseIP(ip));
+                items.add(item);
+            }
+        }
+    }
+
     
+
+
+    
+
+    
+
 }
