424d562271a1ceeb2d6a12e389e1ba3368d55255
Cyrus NOUKPOZOUNKOU <cyrus.noukpozounkou@ens.uvsq.fr>
2025-10-19 02:51:56 +0100

    Ajout de la méthode de test de Dns et correction du fichier config.properties

--- src/test/java/fr/uvsq/cprog/collex/DnsTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsTest.java
@@ -1,5 +1,19 @@
 package fr.uvsq.cprog.collex;
 
+import java.io.IOException;
+
+import org.junit.Test;
+
 public class DnsTest {
+    private final String scope="Test";
+
+    @Test
+    public void DnsTest(){
+        try {
+            Dns dns= new Dns(scope);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
