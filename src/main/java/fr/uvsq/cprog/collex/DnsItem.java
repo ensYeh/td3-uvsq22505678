88baa697c1c4ad8809296bc2f1403065bb90e0e3
Cyrus NOUKPOZOUNKOU <cyrus.noukpozounkou@ens.uvsq.fr>
2025-10-19 01:29:23 +0100

    Ajout des méthodes getNomMachine, getAdresseIP et toString de la classe DnsItem

--- src/main/java/fr/uvsq/cprog/collex/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsItem.java
@@ -8,4 +8,17 @@ public class DnsItem {
         this.nomMachine = nomMachine;
         this.adresseIP = adresseIP;
     }
+
+    public NomMachine getNomMachine() {
+        return nomMachine;
+    }
+
+    public AdresseIP getAdresseIP() {
+        return adresseIP;
+    }
+
+    @Override
+    public String toString() {
+        return adresseIP + " " + nomMachine;
+    }
 }
