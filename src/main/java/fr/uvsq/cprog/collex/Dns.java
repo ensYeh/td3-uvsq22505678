adbc6339dc8f7882731381d5c471250309bd6786
Cyrus NOUKPOZOUNKOU <cyrus.noukpozounkou@ens.uvsq.fr>
2025-10-19 02:42:35 +0100

    Refactoring pour éviter la répétition de codes

--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -11,48 +11,32 @@ public class Dns {
     private List<DnsItem> items = new ArrayList<>();
     private Path filePath;
 
-    public Dns() throws IOException {
-        
-        Properties props = new Properties();
-        try (var fis = Files.newInputStream(Path.of("config.properties"))) {
-            props.load(fis);
-        }
-
-        String dnsFile = props.getProperty("dns.file");
-        if (dnsFile == null) {
-            throw new RuntimeException("Propriété 'dns.file' manquante dans config.properties");
-        }
-        this.filePath = Path.of(dnsFile);
-
-        
-        List<String> lignes = Files.readAllLines(filePath);
-
-        
-        for (String ligne : lignes) {
-            String[] parts = ligne.trim().split("\\s+");
-            if (parts.length == 2) {
-                String nom = parts[0];
-                String ip = parts[1];
-                DnsItem item = new DnsItem(new NomMachine(nom), new AdresseIP(ip));
-                items.add(item);
-            }
-        }
+    public Dns() throws IOException{
+        loadFile("");
+        loadData();
     }
 
     //Ce constructeur prend en compte l'environnement de test
-    public Dns(String scope) throws IOException{
+    public Dns(String scope) throws IOException {
+        loadFile(scope);
+        loadData();
+    }
+
+    private void loadFile(String scope) throws IOException{
         Properties props = new Properties();
         try (var fis = Files.newInputStream(Path.of("config.properties"))) {
             props.load(fis);
         }
 
-        String dnsFile = props.getProperty("dnsTest.file");
+        
+        String dnsFile = props.getProperty("dns"+scope+".file");
         if (dnsFile == null) {
-            throw new RuntimeException("Propriété 'dnsTest.file' manquante dans config.properties");
+            throw new RuntimeException("Propriété 'dns"+scope+".file' manquante dans config.properties");
         }
         this.filePath = Path.of(dnsFile);
+    }
 
-        
+    private void loadData() throws IOException{
         List<String> lignes = Files.readAllLines(filePath);
 
         
