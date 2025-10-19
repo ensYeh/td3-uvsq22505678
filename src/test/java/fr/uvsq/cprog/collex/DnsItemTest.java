15d371abdee2fcc8388b15c6839196c337b4581f
Cyrus NOUKPOZOUNKOU <cyrus.noukpozounkou@ens.uvsq.fr>
2025-10-19 01:45:56 +0100

    Test des méthodes getNomMachine, getAdresseIP et toString de la classe DnsItem

--- src/test/java/fr/uvsq/cprog/collex/DnsItemTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsItemTest.java
@@ -1,5 +1,26 @@
 package fr.uvsq.cprog.collex;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class DnsItemTest {
-    
+    private AdresseIP ip = new AdresseIP("192.168.0.0");
+    private NomMachine machine = new NomMachine("www.uvsq.fr");
+    private DnsItem dnsItem = new DnsItem(machine, ip);
+
+    @Test
+    public void getNomMachineTest(){
+        assertEquals(machine, dnsItem.getNomMachine());
+    }
+
+    @Test
+    public void getAddresseIPTest(){
+        assertEquals(ip, dnsItem.getAdresseIP());
+    }
+
+    @Test
+    public void toStringTest(){
+        assertEquals(ip.toString()+" "+machine.toString(), dnsItem.toString());
+    }
 }
